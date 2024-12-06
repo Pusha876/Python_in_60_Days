90e35c4084abb6deafa694031fa2e41552f5adab
jamie pryce <djpusha876@gmail.com>
2024-12-06 09:39:59 -0500

    Add edit functionality to todo list and update README with match-case and loop explanations

--- Python_Project_1/App1/main.py
+++ Python_Project_1/App1/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, or exit:")
+    user_action = input("Type add, show, edit or exit:")
     user_action = user_action.strip()
 
     match user_action:
@@ -12,5 +12,12 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case 'edit':
+            number = int(input("Enter number of todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo                                                                                                             
         case 'exit':
             break
+
+print("Bye!")
---
+++ Python_Project_1/Day-03/README.md
@@ -0,0 +1,10 @@
+# Day 3: match-case, Break statement, for-loops
+
+### Match-case:
+Is a code block that allows the programmer to match a given value against a series of other values. Match-case is useful when you expect the user to enter different predefined values such as certain commands, days of the week etc.
+
+### Break statement:
+Is an optional part of a while-loop. If the interpreter reaches the break statement, the while-loop will stop executing.
+
+### For-loops:
+Are blocks of code that iterate over objects that are made of other objects (e.g., lists). The loop will perform the same operation over all the items of the list.
