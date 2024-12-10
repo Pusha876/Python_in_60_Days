4a8a346091765f21638b3662a4354dcf6889cb3a
jamie pryce <djpusha876@gmail.com>
2024-12-10 14:58:20 -0500

    Day 6: Implement todo list persistence by adding file read/write functionality and create todos.txt

--- Python_Project_1/App1/main.py
+++ Python_Project_1/App1/main.py
@@ -1,16 +1,27 @@
 # This is a simple todo app that allows you to add, show and edit todos.
 
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit:")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open("todos.txt", "r", encoding="utf-8")
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open("todos.txt", "w", encoding="utf-8")
+            file.writelines(todos)
+            file.close()
         case 'show':
+            file = open("todos.txt", "r", encoding="utf-8")
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item.title()}"
                 print(row)
