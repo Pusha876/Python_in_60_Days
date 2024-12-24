83cbb1c1b80214b119501d7a43cfd9d84a8830b7
jamie pryce <djpusha876@gmail.com>
2024-12-24 12:22:30 -0500

    Day 17: Add file compression functionality: implement GUI for selecting files and destination, and create zip archive utility

--- Python_Project_1/App1/bonus/bonus16_Zip_Files.py
+++ Python_Project_1/App1/bonus/bonus16_Zip_Files.py
@@ -2,22 +2,33 @@
 # and a destination folder.
 
 import FreeSimpleGUI as zf
+from bonus17_zip_creator import make_archive
 
 label1 = zf.Text("Select files to compress: ")
 input1 = zf.Input()
-choose_button1 = zf.FileBrowse("Choose")
+choose_button1 = zf.FileBrowse("Choose", key="files")
 
 label2 = zf.Text("Select destination folder: ")
 input2 = zf.Input()
-choose_button2 = zf.FolderBrowse("Choose")
+choose_button2 = zf.FolderBrowse("Choose", key="folder")
 
 compress_button = zf.Button("Compress")
+output_label = zf.Text(key="output")
 
 window = zf.Window("File Compressor", layout=[
     [label1, input1, choose_button1],
     [label2, input2, choose_button2],
-    [compress_button]
-])
+    [compress_button, output_label]])
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Files compressed successfully.")
+    if event == zf.WIN_CLOSED:
+        break
 
 window.read()
 window.close()
