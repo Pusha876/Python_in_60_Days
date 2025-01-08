4b86c177f0827b32aec5744eda33847833ef4735
jamie pryce <djpusha876@gmail.com>
2025-01-08 13:05:21 -0500

    Day 24Enhance PDF generation: add footer to each page and improve layout for topics in output

--- Python_Project_2/Day-24/App4_pdf_template/main.py
+++ Python_Project_2/Day-24/App4_pdf_template/main.py
@@ -3,15 +3,31 @@ import pandas as pd
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
 
+    # Sets the header
     pdf.set_font("Arial", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 20, 200, 20)
+
+    # Sets the footer
+    pdf.ln(262)
+    pdf.set_font("Arial", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+        # Sets the footer
+        pdf.ln(274)
+        pdf.set_font("Arial", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
 
 pdf.output("output.pdf")
