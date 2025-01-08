313d04ce48c0093508da1b81328730c7913d5e99
jamie pryce <djpusha876@gmail.com>
2025-01-08 17:13:48 -0500

    Day 25: PDF Invoice Generator. Add invoice generation feature: implement script to create PDFs from Excel invoices

---
+++ Python_Project_2/Day-25_invoice_gen/App4_invoice_generator/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("invoices/*xlsx")
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name=None)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_nr = filepath.split("-")[0]
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
