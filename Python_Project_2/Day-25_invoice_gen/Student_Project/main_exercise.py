ba76e12833e78457a3a236fe95dd8ca49c724035
jamie pryce <djpusha876@gmail.com>
2025-01-09 11:56:57 -0500

    Day 26: For Student Exercise - Add code for content reading and display in PDF output

--- Python_Project_2/Day-25_invoice_gen/Student_Project/main_exercise.py
+++ Python_Project_2/Day-25_invoice_gen/Student_Project/main_exercise.py
@@ -13,4 +13,11 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=name, ln=1)
 
+    # Read the content of the file
+    with open(filepath, "r", encoding="utf-8") as file:
+        content = file.read()
+    # Add the content to the pdf
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=10, txt=content)
+
 pdf.output("output.pdf")
