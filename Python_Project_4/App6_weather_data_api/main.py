9a1180b2f57f6e57616a5770a0ebe9014b189d19
jamie pryce <djpusha876@gmail.com>
2025-01-17 12:36:57 -0500

    Day 31: Add new API endpoints for weather data and update home page to display station data

--- Python_Project_4/App6_weather_data_api/main.py
+++ Python_Project_4/App6_weather_data_api/main.py
@@ -4,10 +4,13 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -16,6 +19,24 @@ def about(station, date):
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = float(df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10)
     return {"station": station, "date": date, "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df["year"] = df["    DATE"].dt.year
+    df = df[df["year"] == int(year)]
+    result = df.to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
