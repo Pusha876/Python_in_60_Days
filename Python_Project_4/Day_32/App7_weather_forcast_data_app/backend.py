ff676c1302c6b29753a5ea9ca07b80e7ee04c349
jamie pryce <djpusha876@gmail.com>
2025-01-21 14:08:55 -0500

    Day 33: Refactor weather data retrieval and enhance Streamlit app with error handling and dynamic data visualization

--- Python_Project_4/Day_32/App7_weather_forcast_data_app/backend.py
+++ Python_Project_4/Day_32/App7_weather_forcast_data_app/backend.py
@@ -3,20 +3,18 @@ import requests
 API_key = "0d2413b4a3a8ed58b40ca80b0c435f89"
 
 
-def get_data(place, forecast_days=None, kind=None):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
+def get_data(place, forecast_days=None):
+    url = (
+        f"http://api.openweathermap.org/data/2.5/forecast?q={place}"
+        f"&appid={API_key}"
+    )
     response = requests.get(url)
     data = response.json()
-    num_values = 8 * forecast_days
     filtered_date = data["list"]
+    num_values = 8 * forecast_days
     filtered_date = filtered_date[:num_values]
-    filtered_data = []
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_date]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_date]
-    return filtered_data
+    return filtered_date
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
