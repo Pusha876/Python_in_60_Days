e2c870346556c6254da902d2e9ef370c1d01c5ac
jamie pryce <djpusha876@gmail.com>
2025-01-31 17:28:07 -0500

    Day 38: Add web scraping functionality for music events; implement YAML extraction and error handling for missing configuration file

---
+++ Python_Project_5/App10_web_scraping_music_events/main.py
@@ -0,0 +1,37 @@
+import requests
+import selectorlib
+import os
+
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+        'User-Agent': (
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) '
+            'Chrome/39.0.2171.95 Safari/537.36'
+        )
+    }
+
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source):
+    yaml_file = os.path.join(os.path.dirname(__file__), "extract.yaml")
+    if not os.path.exists(yaml_file):
+        print(f"Error: The file {yaml_file} does not exist.")
+        return None
+
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
