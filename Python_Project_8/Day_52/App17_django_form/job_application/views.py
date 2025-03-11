4f2fb0458d812a14eedc85ddf6e70db918d62f14
jamie pryce <djpusha876@gmail.com>
2025-03-11 16:24:47 -0400

    Day 52: Add Django job application form with initial setup and configuration

---
+++ Python_Project_8/Day_52/App17_django_form/job_application/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
+
+
+def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            Last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+
+            Form.objects.create(
+                first_name=first_name,
+                Last_name=Last_name,
+                email=email,
+                date=date,
+                occupation=occupation
+            )
+
+            message_body = (
+                f"A new job application was submitted by {first_name} "
+                f"{Last_name} on {date} for the position of {occupation}."
+            )
+            email_message = EmailMessage(
+                "Form submission confirmation", message_body, to=[email]
+            )
+            email_message.send()
+
+            messages.success(request, "Form submitted successfully!")
+        else:
+            messages.error(
+                request,
+                'Form submission failed. Please correct the errors and try '
+                'again.'
+            )
+
+    return render(request, "index.html")
